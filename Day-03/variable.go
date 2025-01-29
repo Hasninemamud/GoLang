15a034e0ca4d9686e2aff22ccd3fbbd7b8747aa9
Hasnine Mamud <hasninehemel85@gmail.com>
2025-01-29 23:03:09 +0600

    first commit

---
+++ Day-03/variable.go
@@ -0,0 +1,24 @@
+package main 
+
+import "fmt"
+
+// func main(){
+// 	a := "Hemel"
+// 	fmt.Println(a) 
+// }
+
+func main(){
+	fmt.Print("Enter the name:")
+	var name string 
+	fmt.Scan(&name)
+
+	fmt.Print(("Enter your age: "))
+	var age int 
+	fmt.Scan(&age)
+
+	fmt.Print(("Enter your father age: "))
+	var facterage int 
+	fmt.Scan(&facterage)
+	fmt.Println(age+facterage)
+
+}
